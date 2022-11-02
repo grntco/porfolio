6d02b298e121f3959c20137f0f1b4e59e0e32bb5
Grant Collins <grantcollins03@gmail.com>
2022-11-02 17:03:26 -0400

    Expands card on div click

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-// Dark Mode
-
+// Dark mode
 const themeIcon = document.querySelector('.theme-icon');
 
 themeIcon.addEventListener('click', function() {
@@ -8,3 +7,26 @@ themeIcon.addEventListener('click', function() {
         ? themeIcon.innerHTML = '<i class="far fa-sun"></i>'
         : themeIcon.innerHTML = '<i class="fa-regular fa-moon"></i>';
 });
+
+// Expanding cards to show more info
+const cards = [...document.querySelectorAll('.card')];
+
+cards.forEach(card => {
+    card.addEventListener('click', function(e) {
+        let index = cards.indexOf(e.target);
+        if (index >= 1) {
+            expandCard(cards[index]);
+        } 
+    });
+});
+
+function expandCard(card) {
+    let cardImgs = card.querySelectorAll('.card-img');
+    let moreInfo = card.querySelectorAll('.more-info');
+    cardImgs.forEach(cardImg => {
+        cardImg.classList.toggle('active');
+    });
+    moreInfo.forEach(p => {
+        p.classList.toggle('active');
+    });
+}
