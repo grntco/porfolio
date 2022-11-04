9bf4fd3383d07890f1bb04928161e8f1a1f24274
Grant Collins <grantcollins03@gmail.com>
2022-11-03 21:18:10 -0400

    Working on read more link; commented out

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-// Dark mode
+// Variables
 const themeIcon = document.querySelector('.theme-icon');
+const cards = [...document.querySelectorAll('.card')];
 
+// Dark mode
 themeIcon.addEventListener('click', function() {
     document.body.classList.toggle('dark-mode');
     document.body.classList.contains('dark-mode')
@@ -9,17 +11,6 @@ themeIcon.addEventListener('click', function() {
 });
 
 // Expanding cards to show more info
-const cards = [...document.querySelectorAll('.card')];
-
-cards.forEach(card => {
-    card.addEventListener('click', function(e) {
-        let index = cards.indexOf(e.target);
-        if (index >= 1) {
-            expandCard(cards[index]);
-        } 
-    });
-});
-
 function expandCard(card) {
     let cardImgs = card.querySelectorAll('.card-img');
     let moreInfo = card.querySelectorAll('.more-info');
@@ -29,4 +20,31 @@ function expandCard(card) {
     moreInfo.forEach(p => {
         p.classList.toggle('active');
     });
-}
+};
+
+cards.forEach(card => {
+    card.addEventListener('click', function(e) {
+        let index = cards.indexOf(e.target);
+        console.log(index);
+        if ((index >= 1) || (e.target == document.querySelector('.card-links-list').firstChild)) {
+            expandCard(cards[index]);
+        };
+    });
+});
+
+// const readMore = document.getElementById('')
+
+// readMore.forEach(link => {
+//     link.addEventListener('click', function() {
+//         expandCard();
+//     });
+// });
+
+
+
+
+
+
+
+
+
